8c5b3cea4f1808230b5e871fe90430343a9bcea2
Sergio Ruiz <shecho27@gmail.com>
2020-09-23 20:57:49 -0500

    Creación de vistas con React

---
+++ src/pages/gracias.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { SEO } from "../components"
+import { Button, Purchase } from "../styles/components"
+import { Link } from "gatsby"
+
+export default function gracias() {
+  return (
+    <>
+      <SEO title="Compra exitosa" />
+      <Purchase>
+        <h2>Compra exitosa</h2>
+        <p>Espero que disfrutes tu swag, lucelo con orgullo</p>
+        <p>¡Te esperamos de vuelta, no apres de aprender!</p>
+        <span rol="img" aria-label="emoji">
+          ❤
+        </span>
+        <Link to="/">
+          <Button>Volver al catálogo</Button>
+        </Link>
+      </Purchase>
+    </>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,17 @@
 import React from "react"
 import { Link } from "gatsby"
 import { Jumbo } from "../components"
-import { Layout, SEO } from "../components"
+import { SEO } from "../components"
 
 const IndexPage = () => (
-  <Layout>
+  <>
     <SEO title="Home" />
     <Jumbo />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+    <Link to="/gracias/">Go to Gracias</Link>
+  </>
 )
 
 export default IndexPage
