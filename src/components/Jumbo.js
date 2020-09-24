9482637dcded70ac4e199fec988e7ad80ec4b933
Sergio Ruiz <shecho27@gmail.com>
2020-09-23 22:34:36 -0500

    Definición y uso de plugins en Gatsby

--- src/components/Jumbo.js
+++ src/components/Jumbo.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { StyledJumbo } from "../styles/components"
+import { Image } from "./"
 
 export default function Jumbo({ description }) {
   return (
@@ -8,6 +9,7 @@ export default function Jumbo({ description }) {
         <h2>¡Consigue el mejor swag exclusivo y especial de Platzi!</h2>
         <small>{description}</small>
       </div>
+      <Image name="icon" />
     </StyledJumbo>
   )
 }
