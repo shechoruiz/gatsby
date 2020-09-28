86dc1744bcfbdefd222faf5b90f7b24453d6f422
Sergio Ruiz <shecho27@gmail.com>
2020-09-27 22:47:55 -0500

    Generación de paginas programaticamente, creación del carrito y funcionamiento del mismo

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,21 +1,35 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require("path")
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const productTemplate = path.resolve(`src/templates/Product.js`)
+  const result = await graphql(`
+    query GET_PRODUCTS {
+      allStripeProduct {
+        edges {
+          node {
+            id
+            name
+            metadata {
+              description
+              img
+              wear
+            }
+          }
+        }
+      }
+    }
+  `)
 
-// const path = require('path')
+  if (result.errors) {
+    throw result.errors
+  }
 
-// exports createPages = async({graphql, actions}) => {
-//   const {createPage} = actions
-//   const produtTemplate = path.resolve(`src/templates/Product.js`)
-//   const result = await graphql ``
-
-//   if (result.errors){
-//     throw result.erros
-//   }
-
-//   result.data.allStripeSku.edges
-// }
+  result.data.allStripeProduct.edges.forEach(({ node }) => {
+    createPage({
+      path: `${node.id}`,
+      component: productTemplate,
+      context: node,
+    })
+  })
+}
